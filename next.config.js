543da1a160464ee4ab3399d3dc9f29412147fbb5
Devesh Tibarewal <47741057+DeveshTibarewal@users.noreply.github.com>
2026-01-29 17:43:53 +0530

    Remove basePath and assetPrefix from config
    Removed basePath and assetPrefix from nextConfig.

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
-  basePath: "/Utkal.Web",
-  assetPrefix: "/Utkal.Web/",
 };
 
 module.exports = nextConfig;
